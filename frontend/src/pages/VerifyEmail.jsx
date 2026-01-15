fc1d78b61e017ef219910616a579da5813e4cc1c
ankit gola <ankitgola.eit.22cse010@gmail.com>
2026-01-15 12:19:36 +0530

    Fix verify email link to use FRONTEND_URL

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -7,7 +7,7 @@ const VerifyEmail = () => {
         <div className='bg-white p-8 rounded-2xl shadow-lg w-full max-w-md text-center'>
             <h2 className='text-2xl font-semibold text-green-700 mb-4'>✅ Check Your Email</h2>
             <p className='text-gray-400 text-sm'>
-                We've sent you an email to verify your account. Please check your inbox and click the verification link
+                We've sent you an email to verify your account. Please check your SPAM and click the verification link
             </p>
         </div>
       </div>
